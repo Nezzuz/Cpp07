c8e011cf89840d8f489391c6a923d0360fe56b4f
Julen Leon Larrondo <jleon-la@c3r7s3.42urduliz.com>
2025-06-01 21:40:30 +0200

    I think that it's finished

---
+++ ex02/Array.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+class Array
+{
+	private:
+		T*		array;
+		unsigned int	len;
+
+	public:
+		Array();
+		Array(unsigned int n);
+		Array(const Array &copy);
+		Array &operator= (const Array &model);
+		~Array();
+
+		unsigned int	getLength() const;
+		T&		operator[](unsigned int n);
+		const T&	operator[](unsigned int n) const;
+};
+
+template <typename T>
+Array<T>::Array(): array(NULL), len(0)
+{
+
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n): array(new T[n]), len(n)
+{
+
+}
+
+template <typename T>
+Array<T>::Array(const Array& other): array(NULL), len(0)
+{
+	*this = other;
+}
+
+template <typename T>
+Array<T>	&Array<T>::operator=(const Array<T> &model)
+{
+	unsigned int	i;
+
+	if (this == model)
+		return (*this);
+	delete[] this->array;
+	this->len = model->len;
+	this->array = new T[this->len];
+	i = 0;
+	while (i < this->len)
+	{
+		this->array[i] = model.array[i];
+		i++;
+	}
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	delete[] this->array;
+}
+
+template <typename T>
+unsigned int	Array<T>::getLength() const
+{
+	return (this->len);
+}
+
+template <typename T>
+T&		Array<T>::operator[](unsigned int index)
+{
+	if (index >= this->len)
+		throw std::out_of_range("The index passed as parameter must be in the range");
+	return (this->array[index]);
+}
+
+template <typename T>
+const T&	Array<T>::operator[](unsigned int index) const
+{
+	if (index >= this->len)
+		throw std::out_of_range("The index passed as parameter must be in the range");
+	return (this->array[index]);
+}
+
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,6 +1,21 @@
-#include "iter.hpp"
+#include "Array.hpp"
 
 int	main(void)
 {
+	Array<std::string> array(2);
+	array[0] = "A";
+	array[1] = "B";	
+	try
+	{
+		std::cout << array[0] << std::endl;
+		std::cout << array[1] << std::endl;
+		array[2] = "C";
+		std::cout << array[2] << std::endl;
+	}
+	catch	(std::out_of_range &e)
+	{
+		std ::cout << e.what() << std::endl;
+	}
 	return (0);
 }
+
